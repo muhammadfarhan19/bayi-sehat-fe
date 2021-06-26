14b7d8966291c36e55f7c558355b1b4f4e04d6bb
NgenzSky <39584226+sirianap@users.noreply.github.com>
2021-06-26 11:53:11 +0700

    hapus dummy

--- components/BottomNav.js
+++ components/BottomNav.js
@@ -9,16 +9,16 @@ import Link from "next/link";
 
 function BottomNav(props) {
   return (
-    <div className="mx-5 mb-2 fixed bottom-0 left-0 right-0">
-      <div className="flex w-full justify-center bg-white rounded-full py-3 shadow px-1">
-        <div className="flex-grow text-center text-indigo-500">
-          <Link href="#">
-            <a href="">
+    <div className="mx-5 mb-2 fixed bottom-0 left-0 right-0 lg:hidden">
+      <div className="flex w-full justify-center bg-white border border-gray-100 rounded-full py-3 shadow-2xl px-1">
+        <Link href="/">
+          <a className="flex-grow rounded-full text-center text-indigo-500">
+            
               <HomeIcon className="w-6 h-6 mx-auto" />
               <div className="text-xs">Beranda</div>
-            </a>
-          </Link>
-        </div>
+            
+          </a>
+        </Link>
         <div className="flex-grow text-center text-gray-500">
           <Link href="#">
             <a href="">
@@ -28,15 +28,15 @@ function BottomNav(props) {
           </Link>
         </div>
         <div className="flex-grow text-center text-gray-500">
-          <Link href="#">
+          <Link href="/menu">
             <a href="">
               <TemplateIcon className="w-6 h-6 mx-auto" />
-              <div className="text-xs">Admin</div>
+              <div className="text-xs">Administrasi</div>
             </a>
           </Link>
         </div>
         <div className="flex-grow text-center text-gray-500">
-          <Link href="#">
+          <Link href="/profil">
             <a href="">
               <UserIcon className="w-6 h-6 mx-auto" />
               <div className="text-xs">Profil</div>
