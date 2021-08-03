f421ac0cae359900c75e5cb01e224e528a61e7ac
Daniel Rio Christian <daniel41697@gmail.com>
2021-08-03 19:26:57 +0700

    add page kepangkatan profil

---
+++ components/profil/kepangkatan/ListGolongan.js
@@ -0,0 +1,52 @@
+export default function ListJabatan() {
+    return (
+        <div className="flex ">
+            <div className="-my-2 overflow-x-auto sm:mx-0 ">
+                <div className="py-2 align-start inline-block min-w-full sm:px-0 lg:px-0">
+                    <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                        <table className="w-full rounded-lg bg-gray-100 table-fixed">
+                            <thead className="bg-gray-50">
+                                <tr>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                    >
+                                        JENIS KP
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                    >
+                                        GOL
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                    >
+                                        TMT
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                    >
+                                        MASA KERJA
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                    >
+                                        DOKUMEN SK
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+
+}
