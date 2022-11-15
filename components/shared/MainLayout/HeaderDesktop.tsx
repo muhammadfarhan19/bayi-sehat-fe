cdff8d20408caa71cb687bc40e92b1b256c61dce
Zaqi Akbar <94376170+zaqiakbaar@users.noreply.github.com>
2022-11-15 20:01:31 +0700

    [FIX] Hide BellIcon (#268)
    Co-authored-by: zaqiakbar <117139458+zaqiakbar@users.noreply.github.com>

--- components/shared/MainLayout/HeaderDesktop.tsx
+++ components/shared/MainLayout/HeaderDesktop.tsx
@@ -1,5 +1,9 @@
 import { Popover } from '@headlessui/react';
-import { BellIcon, ChevronDownIcon, MenuIcon } from '@heroicons/react/outline';
+import {
+  // BellIcon,
+  ChevronDownIcon,
+  MenuIcon,
+} from '@heroicons/react/outline';
 import { UserCircleIcon } from '@heroicons/react/solid';
 import Link from 'next/link';
 import * as React from 'react';
@@ -75,13 +79,13 @@ export default function HeaderDesktop(props: NavigationProps) {
 
                 {/* Right section on desktop */}
                 <div className="hidden lg:ml-4 lg:flex lg:items-center lg:pr-0.5">
-                  <button
+                  {/* <button
                     type="button"
                     className="flex-shrink-0 rounded-full p-1 text-indigo-200 hover:bg-white hover:bg-opacity-10 hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
                   >
                     <span className="sr-only">View notifications</span>
                     <BellIcon className="h-6 w-6" aria-hidden="true" />
-                  </button>
+                  </button> */}
 
                   {/* Profile dropdown */}
                   <MenuDropdown navigation={userNavigation}>
